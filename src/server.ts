2b6ce12ab79c90052ad2bb157bbe45062e7bbbec
Robergwillian <roberg.willian@gmail.com>
2021-09-14 10:15:48 -0300

    Adicionado controle para as tags, adicionado middleware para tratar os erros, adicionar um middleware pra verificar se usuario é admin para a criação de tag enquanto não temos o JWT!

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 // importar express de express, se tiver os ... no express
 // utilizar yarn add @types/express -D para adicionar as ependencias de tipo
-import express, { response } from "express"
+import express, { Request, Response, NextFunction } from "express"
+import "express-async-errors"
 import "reflect-metadata"
 import { router } from "./routes"
 import "./database"
@@ -10,8 +11,21 @@ const app = express();
 
 app.use(express.json());
 
-
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error){
+      return response.status(400).json({
+        error: err.message
+      })
+    }
+
+    return response.status(500).json({
+      status: "error",
+      message: "Internal Server Error"
+    })
+
+})
 
 
 //http://localhost:3000
